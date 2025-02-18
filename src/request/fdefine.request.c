6607d4f99f1d2236881b5dcd140287b5746638f2
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-18 14:02:54 -0300

    \tt

--- src/request/fdefine.request.c
+++ src/request/fdefine.request.c
@@ -3,3 +3,25 @@
 //mannaged by silver chain
 #include "../imports/imports.fdeclare.h"
 //silver_chain_scope_end
+
+BearHttpsRequest * newBearHttpsRequest_with_ownership_config(char *route,short route_ownernership_mode){
+   
+}
+
+BearHttpsRequest * newBearHttpsRequest(char *route);
+
+void BearHttpsRequest_set_headder_with_ownership_config(BearHttpsRequest *self ,char *key,short key_ownership_mode,char *value,short value_owner);
+
+void BearHttpsRequest_set_headder(BearHttpsRequest *self ,char *key,char *value);
+
+void BearHttpsRequest_set_body_with_onwership_config(BearHttpsRequest *self ,unsigned char *body,long size,short body_ownership_mode);
+
+void BearHttpsRequest_set_body(BearHttpsRequest *self ,unsigned char *body,long size);
+
+void BearHttpsRequest_set_body_file_stream(BearHttpsRequest *self ,const char *path);
+
+void BearHttpsRequest_set_method(BearHttpsRequest *self ,const char *method);
+
+void BearHttpsRequest_represent(BearHttpsRequest *self);
+
+void BearHttpsRequest_free(BearHttpsRequest *self);
--- src/request/typesC.request.h
+++ src/request/typesC.request.h
@@ -4,9 +4,26 @@
 #include "../imports/imports.typesB.h"
 //silver_chain_scope_end
 
+
+typedef struct private_BearHttpsBodyRequest{
+    unsigned char *body;
+    long size;
+    bool onwer;
+}private_BearHttpsBodyRequest;
+
+typedef struct private_BearHttpsBodyRequestFile{
+    char *path;
+    bool onwer;
+}private_BearHttpsBodyRequestFile;
+
+
 typedef struct BearHttpsRequest{
     char *route;
     bool route_ownernership_mode;
     private_BearHttpsHeadders *headders;
-
+    union{
+        private_BearHttpsBodyRequest *body;
+        private_BearHttpsBodyRequestFile *body_file;
+    }
+    
 }BearHttpsRequest ;
