2a38ee511ef63d1c74074470b0ead8eebbe1f833
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-20 18:28:54 -0300

    adicionando micro client

--- teste.c
+++ teste.c
@@ -4,7 +4,7 @@ int main(){
     BearHttpsRequest *request = newBearHttpsRequest("teste");
     BearHttpsRequest_set_method(request,"POST");
     BearHttpsRequest_add_headder(request, "name", "mateus");
-   // BearHttpsRequest_set_body_file_stream(request,"teste.png");
+    BearHttpsRequest_set_body_file_stream(request,"teste.png");
 
     BearHttpsResponse *response =BearHttpsRequest_fetch(request);
     //BearHttpsRequest_free(request);
