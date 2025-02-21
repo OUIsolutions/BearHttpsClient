f3c548024b202800b91dceaa98369f57604ca40b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-21 07:13:42 -0300

    ATT

--- src/requisition_props/fdefine.requisition_props.c
+++ src/requisition_props/fdefine.requisition_props.c
@@ -6,7 +6,10 @@
 
 private_BearHttpsRequisitionProps * private_new_private_BearHttpsRequisitionProps(const char *route,int default_port){
     int route_size = private_BearsslHttps_strlen(route);
-    if(route_size <sizeof("http://")+1){
+    short HTTP_START_SIZE = sizeof("http://");
+    short HTTPS_START_SIZE = sizeof("https://");
+
+    if(route_size <HTTP_START_SIZE+1){
         return NULL;
     }
  
@@ -18,8 +21,8 @@ private_BearHttpsRequisitionProps * private_new_private_BearHttpsRequisitionProp
         if(default_port == 0){
             self->port = 80;
         }
-        int end = private_BearsslHttps_indexof_from_point(route,'/',sizeof("http://")-1);
-        self->hostname = private_BearsslHttps_strndup(route+sizeof("http://"),end-sizeof("http://"));
+        int end = private_BearsslHttps_indexof_from_point(route,'/',HTTP_START_SIZE-1);
+        self->hostname = private_BearsslHttps_strndup(route+HTTP_START_SIZE,end-HTTP_START_SIZE);
         self->type = BEARSSL_HTTP_REQUISITION_TYPE;
     }
 
@@ -27,8 +30,8 @@ private_BearHttpsRequisitionProps * private_new_private_BearHttpsRequisitionProp
         if(default_port == 0){
             self->port = 443;
         }
-        int end = private_BearsslHttps_indexof_from_point(route,'/',sizeof("https://")-1);
-        self->hostname = private_BearsslHttps_strndup(route+sizeof("https://"),end-sizeof("https://"));
+        int end = private_BearsslHttps_indexof_from_point(route,'/',HTTPS_START_SIZE-1);
+        self->hostname = private_BearsslHttps_strndup(route+HTTPS_START_SIZE,end-HTTPS_START_SIZE);
         self->type = BEAR_HTTPS_HTTPS_REQUISITION_TYPE;
     }
 
