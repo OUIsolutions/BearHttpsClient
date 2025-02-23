00fc19ce12437d4869502573f625868060cff270
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-23 03:20:31 -0300

    att

--- src/request/fdefine.fetch_https.c
+++ src/request/fdefine.fetch_https.c
@@ -14,5 +14,5 @@ void BearHttpsRequest_fetch_https(BearHttpsRequest *self, BearHttpsResponse *res
         unsigned char iobuf[BR_SSL_BUFSIZE_BIDI];
 	br_ssl_engine_set_buffer(&sc.eng, iobuf, sizeof iobuf, 1);
         br_ssl_client_reset(&sc,hostname, 0);
-        br_sslio_init(&ioc, &sc.eng, private_BearHttps_sock_read, &fd, private_BearHttps_sock_write, &fd);
+        br_sslio_init(&ioc, &sc.eng, private_BearHttps_sock_read, &connection_file_descriptor, private_BearHttps_sock_write, &connection_file_descriptor);
 }
