805ae08dedb5c4eab101e1648e7d5cb16b7c0ae2
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-26 20:18:30 -0300

    att

--- src/response/fdefine.read_write.c
+++ src/response/fdefine.read_write.c
@@ -93,15 +93,19 @@ unsigned char *BearHttpsResponse_read_body(BearHttpsResponse *self,long max_size
         }
 
 
+        
         long readded = private_BearHttpsResponse_read_chunck_raw(self,buffer,size_to_read);
-        printf("--readded: %ld\n",readded);
+        
+        
+        if(readded < 0){
+            break;
+        }
+
         if(readded == 0){
             break;
         }
 
-        if(readded < 0){
-            break;
-        }
+        
 
         self->body_readded += readded;
         self->body_size += readded;
