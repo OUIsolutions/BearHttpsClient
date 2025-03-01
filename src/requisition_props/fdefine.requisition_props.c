a13ac77d5b2592a69473b07334b898a092c1b3e6
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-01 00:30:44 -0300

    at

--- src/requisition_props/fdefine.requisition_props.c
+++ src/requisition_props/fdefine.requisition_props.c
@@ -4,8 +4,8 @@
 #include "../imports/imports.fdeclare.h"
 //silver_chain_scope_end
 
-private_BearHttpsRequisitionProps * private_new_private_BearHttpsRequisitionProps(const char *route,int default_port){
-    int url_size = private_BearsslHttps_strlen(route);
+private_BearHttpsRequisitionProps * private_new_private_BearHttpsRequisitionProps(const char *url,int default_port){
+    int url_size = private_BearsslHttps_strlen(url);
     const short HTTP_START_SIZE = sizeof("http://");
     const short HTTPS_START_SIZE = sizeof("https://");
 
@@ -17,13 +17,13 @@ private_BearHttpsRequisitionProps * private_new_private_BearHttpsRequisitionProp
     short start_size;
 
     //setuping defaults elements
-    if(private_BearsslHttps_startswith(route,"http://")){
+    if(private_BearsslHttps_startswith(url,"http://")){
         self->port = default_port ?  default_port: 80;
         self->type = BEARSSL_HTTP_REQUISITION_TYPE;
         start_size = HTTP_START_SIZE;
     }
 
-    else if(private_BearsslHttps_startswith(route,"https://")){
+    else if(private_BearsslHttps_startswith(url,"https://")){
         self->port = default_port ?  default_port: 443;
         self->type = BEAR_HTTPS_HTTPS_REQUISITION_TYPE;
         start_size = HTTPS_START_SIZE;
@@ -35,20 +35,43 @@ private_BearHttpsRequisitionProps * private_new_private_BearHttpsRequisitionProp
     }
 
     //1000 //2000
-    int end_host_name_and_start_of_route = private_BearsslHttps_indexof_from_point(route,'/',start_size-1);
+    int end_host_name_and_start_of_route = private_BearsslHttps_indexof_from_point(url,'/',start_size-1);
 
     if(end_host_name_and_start_of_route == -1){
         end_host_name_and_start_of_route = url_size;
     }
 
-    self->hostname = private_BearsslHttps_strndup((route+start_size-1),end_host_name_and_start_of_route-start_size+1);
+    self->hostname = private_BearsslHttps_strndup((url+start_size-1),end_host_name_and_start_of_route-start_size+1);
     long host_name_size = private_BearsslHttps_strlen(self->hostname);
 
     if(end_host_name_and_start_of_route == url_size){
         self->route = private_BearsslHttps_strdup("/");    }
     else{
-         self->route = private_BearsslHttps_strndup((route+end_host_name_and_start_of_route),url_size-end_host_name_and_start_of_route+1);
+         self->route = private_BearsslHttps_strndup((url+end_host_name_and_start_of_route),url_size-end_host_name_and_start_of_route+1);
     }
+    
+    //iterate over hostname to determine if its ipv4
+    short total_dots = 0;
+    bool only_numbers = true;
+    for(int i = 0; i < host_name_size; i++){
+        if(self->hostname[i] == '.'){
+            total_dots++;
+            continue;
+        }
+        if(self->hostname[i] == ':'){
+            break;
+        }
+        if(self->hostname[i] < '0' || self->hostname[i] > '9'){
+            only_numbers = false;
+            break;
+        }
+        
+    }
+
+    if(total_dots == 3 && only_numbers){
+        self->is_ipv4 = true;
+    }
+
     //detect if self.hostname its ipv4
     if(default_port == 0){
     int start_port = private_BearsslHttps_indexof_from_point(self->hostname,':',0);
