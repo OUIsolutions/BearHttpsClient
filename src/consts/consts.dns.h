af9036d2c4db2dee90229059d019809541def600
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-02 09:53:16 -0300

    att

--- src/consts/consts.dns.h
+++ src/consts/consts.dns.h
@@ -4,5 +4,9 @@
 #include "../imports/imports.typesH.h"
 //silver_chain_scope_end
 
-#define DNS_SERVER_IP "8.8.8.8"
-#define DNS_SERVER_HOSTNAME "dns.google.com"
+BearHttpsClientDnsProvider privateBearHttpsProviders[] = {
+    {.hostname= "dns.google.com",.route="/resolve",.ip = "8.8.8.8"}
+};
+
+
+int privateBearHttpsProvidersSize = sizeof(privateBearHttpsProviders)/sizeof(BearHttpsClientDnsProvider);
