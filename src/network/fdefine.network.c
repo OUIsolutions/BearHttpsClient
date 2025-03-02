2308b54bf161e8b5519764381225cb8176fbeffa
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-02 13:41:55 -0300

    att

--- src/network/fdefine.network.c
+++ src/network/fdefine.network.c
@@ -108,7 +108,7 @@ static int private_BearHttpsRequest_connect_host(BearHttpsResponse *response, co
           
             BearHttpsClientDnsProvider provider = chosen_dns_providers[i];
             BearHttpsRequest *dns_request = newBearHttpsRequest_fmt("https://%s:%d%s?name=%s&type=A",provider.ip,provider.port, provider.route, host); 
-            printf("used url %s\n",dns_request->url);
+           //printf("used url %s\n",dns_request->url);
             dns_request->custom_bear_dns = provider.hostname;
 
             //these its require, otherwise can generate indirect recursion error if provider.ip its wrong
