926dc46e91e44fa54b3d80af6103ee37de486199
michaelangel007 <michaelangel007@sharedcraft.com>
2025-03-03 07:26:31 -0800

    Add MSVC compilation support to modules.

--- src/src_dependencies/dep_declare.dependencies.h
+++ src/src_dependencies/dep_declare.dependencies.h
@@ -17,6 +17,16 @@
 #define BearsslHttps_free free
 #endif
 
+#if defined(_MSC_VER)
+    #include <BaseTsd.h>
+    #include "intrin.h"
+
+    typedef SSIZE_T ssize_t;
+    typedef __m128i __m128i_u;
+
+    #define __src_inner_hbyteswap_ulong _byteswap_ulong
+#endif
+
 #include "../../dependencies/UniversalSocket.h"
 #define BR_ENABLE_INTRINSICS   1
 #include "../../dependencies/BearSSLSingleUnit.h"
