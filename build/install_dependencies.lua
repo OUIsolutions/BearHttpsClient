9c46fd854a0f1587a80ad81c545d878862101f48
Mateus Moutinho Queiroz <61179749+mateusmoutinho@users.noreply.github.com>
2025-03-08 00:46:55 -0300

    att

--- build/install_dependencies.lua
+++ build/install_dependencies.lua
@@ -1,7 +1,7 @@
 function install_dependencies()
     local hasher = darwin.dtw.newHasher()
     hasher.digest_folder_by_content("dependencies")
-    local EXPECTED_SHA = '0a705ecc581f612a97dbbb2f5768f6caacfab24e5d223e9ea5a5276d00513ac0'
+    local EXPECTED_SHA = '76163fc7939200269ecdb2d9cbf817fbf715503cad34355557a944cd0485d1ed'
     if hasher.get_value() == EXPECTED_SHA then
         return
     end
@@ -28,7 +28,7 @@ function install_dependencies()
 
     darwin.dtw.remove_any("BearSSL")
     os.execute(
-        "curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.6/UniversalSocket.c -o dependencies/UniversalSocket.c")
+        "curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.7/UniversalSocket.c -o dependencies/UniversalSocket.c")
     os.execute(
         "curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.7/UniversalSocket.h -o dependencies/UniversalSocket.h")
 
