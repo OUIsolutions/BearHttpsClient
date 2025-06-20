345fe7f6d6e1298d92890cd9b7c1d554e00cfa33
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-20 01:19:26 -0300

    att

--- src/network/globals.cache_dns.h
+++ src/network/globals.cache_dns.h
@@ -1,3 +1,7 @@
+//silver_chain_scope_start
+//mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
+#include "../imports/imports.fdeclare.h"
+//silver_chain_scope_end
 
 privateBearHttpsDnsCache privateBearHttpsDnsCache_itens[BEARSSL_DNS_CACHE_SIZE] ={0};
 
