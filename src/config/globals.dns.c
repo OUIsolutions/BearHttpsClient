8e6a024ce4e84a0b2eb8ab4c946d49abbd664bdf
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-20 11:58:24 -0300

    syy

--- src/config/globals.dns.c
+++ src/config/globals.dns.c
@@ -7,8 +7,8 @@
 
 BearHttpsClientDnsProvider privateBearHttpsProviders[] = {
     {.hostname= "dns.google.com",.route="/resolve",.ip = "8.8.8.8",.port=443},
-    {.hostname= "dns.quad9.net",.route="/dns-query",.ip = "9.9.9.9",.port=5053},
     {.hostname= "dns.nextdns.io",.route="/dns-query",.ip = "217.146.9.93",.port=443},
+    {.hostname= "dns.quad9.net",.route="/dns-query",.ip = "9.9.9.9",.port=5053},
 
    
 };
