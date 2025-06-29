94c9bf11396fd98ee61956b3bb458b638edfc587
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-06-28 22:03:52 -0300

    att

--- src/response/body_read_chunk/fdeclare.body_read_chunk.h
+++ src/response/body_read_chunk/fdeclare.body_read_chunk.h
@@ -3,4 +3,11 @@
 #include "../../imports/imports.typesH.h"
 //silver_chain_scope_end
 
+
+int BearHttpsResponse_read_body_chunck_http1(BearHttpsResponse *self,unsigned char *buffer,long size);
+
+
+
+int BearHttpsResponse_read_body_chunck_raw(BearHttpsResponse *self,unsigned char *buffer,long size);
+
 int BearHttpsResponse_read_body_chunck(BearHttpsResponse *self,unsigned char *buffer,long size);
