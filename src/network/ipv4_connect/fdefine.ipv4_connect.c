d89f4298fa1cd5c7b755985f5b7643a1b8a35012
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-10 07:55:21 -0300

    start working

--- src/network/ipv4_connect/fdefine.ipv4_connect.c
+++ src/network/ipv4_connect/fdefine.ipv4_connect.c
@@ -5,11 +5,15 @@
 
 static int private_BearHttpsRequest_connect_ipv4(BearHttpsResponse *self, const char *ipv4_ip, int port) {
     printf("connecting to ipv4 %s:%d\n", ipv4_ip, port);
-    int sockfd = Universal_socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd = Universal_socket(UNI_AF_INET, UNI_SOCK_STREAM, 0);
     if (sockfd < 0) {
         BearHttpsResponse_set_error(self,"ERROR: failed to create socket",BEARSSL_HTTPS_FAILT_TO_CREATE_SOCKET);
         return -1; 
     }
+
+    // Set socket to non-blocking mode
+    int flags = fcntl(sockfd, F_GETFL, 0);
+    fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
 
     Universal_sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
@@ -22,11 +26,40 @@ static int private_BearHttpsRequest_connect_ipv4(BearHttpsResponse *self, const 
         return -1;
     }
 
-    if (Universal_connect(sockfd, (Universal_sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    int ret = Universal_connect(sockfd, (Universal_sockaddr *)&server_addr, sizeof(server_addr));
+    if (ret < 0 && errno != EINPROGRESS) {
         BearHttpsResponse_set_error(self,"ERROR: failed to connect",BEARSSL_HTTPS_FAILT_TO_CONNECT);
         Universal_close(sockfd); 
         return -1;
     }
+
+    // Use select to wait for connection with timeout
+    fd_set write_fds;
+    FD_ZERO(&write_fds);
+    FD_SET(sockfd, &write_fds);
+    
+    struct timeval timeout;
+    timeout.tv_sec = 1;  
+    timeout.tv_usec = 0;
+    
+    ret = select(sockfd + 1, NULL, &write_fds, NULL, &timeout);
+    if (ret <= 0) {
+        BearHttpsResponse_set_error(self,"ERROR: connection timeout or failed",BEARSSL_HTTPS_FAILT_TO_CONNECT);
+        Universal_close(sockfd);
+        return -1;
+    }
+
+    // Check if connection succeeded
+    int error = 0;
+    socklen_t len = sizeof(error);
+    if (getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, &len) < 0 || error != 0) {
+        BearHttpsResponse_set_error(self,"ERROR: failed to connect",BEARSSL_HTTPS_FAILT_TO_CONNECT);
+        Universal_close(sockfd);
+        return -1;
+    }
+
+    // Set socket back to blocking mode
+    fcntl(sockfd, F_SETFL, flags);
 
     return sockfd;
 }
