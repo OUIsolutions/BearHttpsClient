b752cd42a337350c00268a85163551a172567cd1
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-11 17:52:53 -0300

    added string array

---
+++ src/StringArray/types.StringArray.h
@@ -0,0 +1,8 @@
+//silver_chain_scope_start
+//mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
+#include "../imports/imports.macros.h"
+//silver_chain_scope_end
+typedef struct privateBearHttpsStringArray {
+    int size;
+    char **strings;
+}privateBearHttpsStringArray;
