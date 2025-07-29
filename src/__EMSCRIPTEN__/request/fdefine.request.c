cb68b68b50e43898e01e8513fce3260b0d412f2c
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-29 03:07:34 -0300

    att

--- src/__EMSCRIPTEN__/request/fdefine.request.c
+++ src/__EMSCRIPTEN__/request/fdefine.request.c
@@ -3,4 +3,14 @@
 #include "../../imports/imports.dep_define.h"
 //silver_chain_scope_end
 #if defined(__EMSCRIPTEN__)
+
+BearHttpsRequest * newBearHttpsRequest_with_url_ownership_config(char *url,short url_ownership_mode){
+    BearHttpsRequest *self = (BearHttpsRequest *)malloc(sizeof(BearHttpsRequest));
+    *self = (BearHttpsRequest){0};
+    BearHttpsRequest_set_url_with_ownership_config(self,url,url_ownership_mode);
+    self->headers = private_newBearHttpsHeaders();
+    self->body_type =PRIVATE_BEARSSL_NO_BODY;
+    private_BearsslHttps_strcpy(self->method,"GET");
+    return self;
+}
 #endif 
