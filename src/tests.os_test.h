809d2ff77a4823d283cc7e0acd0ea4098c15da59
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-29 03:32:37 -0300

    att

--- src/tests.os_test.h
+++ src/tests.os_test.h
@@ -3,5 +3,10 @@
 
 // Discoment these to allo emcc on your intelisense
 // never Discoment these for building the lib
-//#define __EMSCRIPTEN__ 
+#define __EMSCRIPTEN__ 
 #endif 
+#if !defined(_WIN32) 
+// Discoment these to allo emcc on your intelisense
+// never Discoment these for building the lib
+//#define _WIN32
+#endif
