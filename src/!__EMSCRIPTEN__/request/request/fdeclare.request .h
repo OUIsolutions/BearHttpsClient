c76c828f246e64f286f547b6412632958d8a8233
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-29 02:20:30 -0300

    reconstruct struct implementations

--- src/!__EMSCRIPTEN__/request/request/fdeclare.request .h
+++ src/!__EMSCRIPTEN__/request/request/fdeclare.request .h
@@ -5,25 +5,6 @@
 #if !defined(__EMSCRIPTEN__)
 
 
-
-BearHttpsRequest * newBearHttpsRequest_with_url_ownership_config(char *url,short url_ownership_mode);
-
-BearHttpsRequest * newBearHttpsRequest(const char *url);
-
-BearHttpsRequest * newBearHttpsRequest_fmt(const char *url,...);
-
-void BearHttpsRequest_set_url_with_ownership_config(BearHttpsRequest *self , char *url,short url_ownership_mode);
-
-void BearHttpsRequest_set_url(BearHttpsRequest *self ,const char *url);
-
-
-void BearHttpsRequest_add_header_with_ownership_config(BearHttpsRequest *self ,char *key,short key_ownership_mode,char *value,short value_owner);
-
-void BearHttpsRequest_add_header(BearHttpsRequest *self ,const char *key,const char *value);
-
-void BearHttpsRequest_add_header_fmt(BearHttpsRequest *self,const char *key,const char *format,...);
-
-
 void BearHttpsRequest_set_http_protocol(BearHttpsRequest *self ,short protocol);
 
 
@@ -39,11 +20,8 @@ void BearHttpsRequest_set_chunk_header_read_props(BearHttpsRequest *self ,int ch
 void BearHttpsRequest_set_trusted_anchors(BearHttpsRequest *self ,br_x509_trust_anchor *trust_anchors, size_t trusted_anchors_size);
 
 
-void BearHttpsRequest_set_method(BearHttpsRequest *self ,const char *method);
-
-void BearHttpsRequest_represent(BearHttpsRequest *self);
 
 
-void BearHttpsRequest_free(BearHttpsRequest *self);
+
 
 #endif
