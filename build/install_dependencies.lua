74bd5a44693fa0ec90d7e48def160b157b1b8b76
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-06 05:49:18 -0300

    deps

--- build/install_dependencies.lua
+++ build/install_dependencies.lua
@@ -1,42 +1,41 @@
-function install_dependencies()
-    local hasher = darwin.dtw.newHasher()
-    hasher.digest_folder_by_content("dependencies")
-    local EXPECTED_SHA = '3f167f43e58b26b18e2a48517ad921194f6822562037f21527c10fdbe6228a57'
-    if hasher.get_value() == EXPECTED_SHA then
-        return
+DEPS = {
+    {url = "https://github.com/OUIsolutions/BearSslSingle-Unit/releases/download/0.0.3/BearSSLSingleUnit.h", dest = "dependencies/BearSSLSingleUnit.h"},
+    {url = "https://github.com/OUIsolutions/BearSslSingle-Unit/releases/download/0.0.3/BearSSLSingleUnit.c", dest = "dependencies/BearSSLSingleUnit.c"},
+    {url = "https://raw.githubusercontent.com/DaveGamble/cJSON/refs/tags/v1.7.18/cJSON.c", dest = "dependencies/cJSON.c"},
+    {url = "https://raw.githubusercontent.com/DaveGamble/cJSON/refs/tags/v1.7.18/cJSON.h", dest = "dependencies/cJSON.h"},
+    {url = "https://github.com/OUIsolutions/C2Wasm/releases/download/0.6.0/c2wasm.c", dest = "dependencies/c2wasm.c"},
+    {url = "https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.8/UniversalSocket.c", dest = "dependencies/UniversalSocket.c"},
+    {url = "https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.8/UniversalSocket.h", dest = "dependencies/UniversalSocket.h"},
+    {url = "https://github.com/OUIsolutions/BearSSLTrustedAnchors/releases/download/2025-8-6/BearSSLTrustAnchors.c", dest = "dependencies/BearSSLTrustAnchors.c"},
+}
+
+-- Downloads once, then reuses the result!
+local cache_curl = darwin.dtw.create_cache_function({
+    expiration = 300,  -- 5 minutes
+    cache_dir = "./web_cache/",
+    cache_name = "libs",  -- Added cache_name
+    callback = function(url)
+        os.execute("curl -L " .. url .. " -o temp")
+        local content =  darwin.dtw.load_file("temp")
+        darwin.dtw.remove_any("temp")
+        return content
     end
+})
+
+
+
+function install_dependencies()
+ 
 
     darwin.dtw.remove_any("dependencies")
 
-    os.execute("mkdir -p dependencies")
-    os.execute(
-        "curl -L https://github.com/OUIsolutions/BearSslSingle-Unit/releases/download/0.0.3/BearSSLSingleUnit.h -o dependencies/BearSSLSingleUnit.h ")
-    os.execute(
-        "curl -L https://github.com/OUIsolutions/BearSslSingle-Unit/releases/download/0.0.3/BearSSLSingleUnit.c -o dependencies/BearSSLSingleUnit.c")
+    for i=1 ,#DEPS do
+        local dep = DEPS[i]
+        local dest = dep.dest
+        local content = cache_curl(dep.url)
+        darwin.dtw.write_file(dest, content)
+    end
 
-    os.execute(
-    "curl -L https://raw.githubusercontent.com/DaveGamble/cJSON/refs/tags/v1.7.18/cJSON.c -o dependencies/cJSON.c")
-    os.execute(
-    "curl -L https://raw.githubusercontent.com/DaveGamble/cJSON/refs/tags/v1.7.18/cJSON.h -o dependencies/cJSON.h")
-    ---- trusted anchors
-    os.execute("curl -L https://github.com/OUIsolutions/C2Wasm/releases/download/0.6.0/c2wasm.c -o dependencies/c2wasm.c")
   
-    darwin.dtw.remove_any("BearSSL")
-    os.execute("git clone https://www.bearssl.org/git/BearSSL")
-    os.execute("curl -L https://curl.se/ca/cacert.pem -o BearSSL/cacert.pem")
-    os.execute("cd BearSSL && make")
-    os.execute("chmod +x BearSSL/build/brssl")
-    os.execute("./BearSSL/build/brssl ta BearSSL/cacert.pem > dependencies/BearSSLTrustAnchors.c")
 
-    darwin.dtw.remove_any("BearSSL")
-    os.execute(
-        "curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.8/UniversalSocket.c -o dependencies/UniversalSocket.c")
-    os.execute(
-        "curl -L https://github.com/SamuelHenriqueDeMoraisVitrio/UniversalSocket/releases/download/v0.2.8/UniversalSocket.h -o dependencies/UniversalSocket.h")
-
-
-
-    local new_hasher = darwin.dtw.newHasher()
-    new_hasher.digest_folder_by_content("dependencies")
-    print("new hasher is: " .. new_hasher.get_value())
 end
