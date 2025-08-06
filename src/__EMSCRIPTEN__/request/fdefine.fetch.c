f7c077ebe18b34d3bcb27159dfd8d71c1b59d47b
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-06 02:37:55 -0300

    att

--- src/__EMSCRIPTEN__/request/fdefine.fetch.c
+++ src/__EMSCRIPTEN__/request/fdefine.fetch.c
@@ -3,7 +3,7 @@
 #include "../../imports/imports.dep_defineB.h"
 //silver_chain_scope_end
 #if defined(__EMSCRIPTEN__) 
-
+#include <unistd.h>
 BearHttpsResponse * BearHttpsRequest_fetch(BearHttpsRequest *self){
 
     c2wasm_js_var headers = c2wasm_create_object();
@@ -21,20 +21,8 @@ BearHttpsResponse * BearHttpsRequest_fetch(BearHttpsRequest *self){
     c2wasm_append_array_any(args_to_cal, props);
 
     c2wasm_js_var js_response = async_c2wasm_call_object_prop(c2wasm_window, "fetch", args_to_cal);
-    BearHttpsResponse *response = private_newBearHttpsResponse();
-    if(c2wasm_get_var_type(js_response) == c2wasm_null){
-        printf("its a null\n");
-        return response;
-    }
-    
-    if(c2wasm_instance_of(js_response,c2wasm_error)){
-        printf("its a error\n");
-        return response;
-    }
 
 
-    printf("its not a error\n");
-    c2wasm_show_var_on_console(js_response);
     return NULL;
 }
 #endif 
