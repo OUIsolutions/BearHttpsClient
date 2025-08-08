49286b50ddc0754e406a13280d0408ccd2f3337f
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-08 04:45:35 -0300

    att

--- src/__unix__&&!__EMSCRIPTEN__/private_dep_declare.h
+++ src/__unix__&&!__EMSCRIPTEN__/private_dep_declare.h
@@ -2,5 +2,8 @@
 //mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
 #include "../imports/imports.private_macros.h"
 //silver_chain_scope_end
+#if defined(__unix__)  && !defined(__EMSCRIPTEN__)
 
 #include <unistd.h>
+#include <fcntl.h>
+#endif 
